14ff0c8b1c1a0184334e0e5bd856b99e4d98b989
Ali Bulut <ali@alibulut.net>
2021-08-20 18:20:58 +0300

    consuming GetAllProducts service method in console app

--- ProductGrpcClient/Program.cs
+++ ProductGrpcClient/Program.cs
@@ -13,10 +13,20 @@ namespace ProductGrpcClient
             var client = new ProductProtoService.ProductProtoServiceClient(channel);
 
             Console.WriteLine("GetProductAsync Started...");
-
             var response = await client.GetProductAsync(new GetProductRequest { ProductId = 1 });
-
             Console.WriteLine("GetProductAsync Response: " + response.ToString());
+
+
+            Console.WriteLine("GetAllProductsAsync Started...");
+            using(var clientData = client.GetAllProducts(new GetAllProductsRequest()))
+            {
+                while (await clientData.ResponseStream.MoveNext(new System.Threading.CancellationToken()))  
+                {
+                    var currentProduct = clientData.ResponseStream.Current;
+                    Console.WriteLine("GetAllProductsAsync Response: " + currentProduct.ToString());
+                }
+            }
+
             Console.ReadKey();
         }
     }
