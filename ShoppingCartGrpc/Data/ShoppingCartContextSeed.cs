82ee56976529ca7581528bfdb118eae9451f2956
Ali Bulut <ali@alibulut.net>
2021-08-21 14:04:38 +0300

    creating seed data mechanism for ShoppingCart model

---
+++ ShoppingCartGrpc/Data/ShoppingCartContextSeed.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ShoppingCartGrpc.Models;
+
+namespace ShoppingCartGrpc.Data
+{
+    public class ShoppingCartContextSeed
+    {
+        public static void SeedAsync(ShoppingCartContext shoppingCartContext)
+        {
+            if (!shoppingCartContext.ShoppingCarts.Any())
+            {
+                var shoppingCarts = new List<ShoppingCart>
+                {
+                    new ShoppingCart
+                    {
+                        Username = "swn",
+                        Items = new List<ShoppingCartItem>
+                        {
+                            new ShoppingCartItem
+                            {
+                                ProductId = 2,
+                                ProductName = "Macbook Pro M1 13 inch",
+                                Price = 1299,
+                                Quantity = 2,
+                                Color = "Black"
+                            },
+                            new ShoppingCartItem
+                            {
+                                ProductId = 3,
+                                ProductName = "Airpods Pro",
+                                Price = 199,
+                                Quantity = 1,
+                                Color = "White"
+                            }
+                        }
+                    },
+                };
+                shoppingCartContext.ShoppingCarts.AddRange(shoppingCarts);
+                shoppingCartContext.SaveChanges();
+            }
+        }
+    }
+}
--- ShoppingCartGrpc/Program.cs
+++ ShoppingCartGrpc/Program.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShoppingCartGrpc.Data;
 
 namespace ShoppingCartGrpc
 {
@@ -13,7 +15,21 @@ namespace ShoppingCartGrpc
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            SeedDatabase(host);
+
+            host.Run();
+        }
+
+        private static void SeedDatabase(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            var shoppingCartContext = services.GetRequiredService<ShoppingCartContext>();
+
+            ShoppingCartContextSeed.SeedAsync(shoppingCartContext);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
